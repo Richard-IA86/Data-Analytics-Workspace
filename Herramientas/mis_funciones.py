2741c238c6bc399f06eadbfb79508fbd339f94bf
Richard_IA86 <Richard.r.ia86@gmail.com>
2026-04-01 21:34:51 -0300

    style: parte comentario largo en mis_funciones.py (E501)

--- Herramientas/mis_funciones.py
+++ Herramientas/mis_funciones.py
@@ -1,3 +1,4 @@
-# Aquí iremos guardando las funciones y herramientas de Python que vayamos creando
+# Aquí iremos guardando las funciones y herramientas de Python
+# que vayamos creando
 def ejemplo_funcion():
     print("¡Herramientas listas!")
